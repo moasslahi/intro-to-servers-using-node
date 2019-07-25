2e7fd10dafa4f57429b7154629323aa9e26bbd34
Mohammed Asslahi <asslahi37@gmail.com>
2019-07-25 17:37:02 +0300

    express middleware intro

--- node/server.js
+++ node/server.js
@@ -1,44 +1,73 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-	console.log('headers',request.headers)
-	console.log('methods',request.method)
-	console.log('url',request.url)
-	const user = {
-		name: 'mo',
-		hobby: 'footy'
-	}
+// const server = http.createServer((request, response) => {
+// 	console.log('headers',request.headers)
+// 	console.log('methods',request.method)
+// 	console.log('url',request.url)
+// 	const user = {
+// 		name: 'mo',
+// 		hobby: 'footy'
+// 	}
 
-	response.setHeader('content-Type', 'application/json');
-	response.end(JSON.stringify(user))
-})
+// 	response.setHeader('content-Type', 'application/json');
+// 	response.end(JSON.stringify(user))
+// })
 
-server.listen(3000);
-
-
+// server.listen(3000);
 
 
-// server using express.js
+
+// simple express server 
 
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {               // if u get this route do this else do the next rout 
-	res.send("getting root ");
-});
+app.use((req,res, next) => {
+	console.log('<h1>hello</h1>')
+	next();
+})  // middleware recieves ahead of time and modifies it and then passes it to the next() parameter
 
-app.get('/profile', (req, res) => {
-	res.send("getting profile ");
-});
 
-app.delete('/profile', (req, res) => {
+app.get('/', (req,res) => {
+	res.send('test')
+})
 
-	const user = {
-		name: 'mo',
-		hobby: 'footy'
-	}
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // server using express.js
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {               // if u get this route do this else do the next rout 
+// 	res.send("getting root ");
+// });
+
+// app.get('/profile', (req, res) => {
+// 	res.send("getting profile ");
+// });
+
+// app.delete('/profile', (req, res) => {
+
+// 	const user = {
+// 		name: 'mo',
+// 		hobby: 'footy'
+// 	}
 	
-	res.send(user);
-});
-app.listen(3000)
+// 	res.send(user);
+// });
+// app.listen(3000)
